5833555ac88e98ae7a2dd1951cbf1a97df88981b
kimcsmn <kimcsmn@gmail.com>
2021-01-06 03:27:46 +0800

    added search input functionality

--- src/App.js
+++ src/App.js
@@ -7,26 +7,48 @@ function App() {
   const APP_KEY = "8a8f88177c98615233f03555bca75607";
   const APP_ID = "bc7f7b0b";
 
+  // state hooks
   const [recipes, setRecipes] = useState([]);
 
+  // state of search input
+  const [keyword , setKeyword] = useState("");
+
+  // state of final input
+  const [query, setQuery] = useState("chicken");
+
+
+  // execute getRecipe whenever query is updated
   useEffect(() => {
     getRecipe();
   }, []);
 
+  // fetch the data from api
   const getRecipe = async () => {
     const res = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await res.json();
     setRecipes(data.hits);
   }
 
+
+  const handleChange = (e) => {
+    setKeyword(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setQuery(keyword);
+    console.log({keyword, query});
+    setKeyword("");
+  }
+
     return(
       <div className="App">
-        <form className="form">
-          <input type="text" />
-          <button type="submit">Search</button>
+        <form className="form" onSubmit={handleSubmit}>
+          <input type="text" value={keyword} onChange={handleChange}/>
+          <button type="submit" >Search</button>
         </form>
 
-      {/* loop through recipes and each recipe will instantiate a Recipe component */}
+      {/* loop through recipes and each recipe will instantiate a Recipe component with props */}
       <div className="Recipe">
         {
          recipes.map(r => {
