a8eea3a46c2b027a66ec244db41463c6d79718fa
kimcasamina <kim.csmn@gmail.com>
2021-01-16 00:54:35 +0800

    updated nutrients.js

--- src/components/Nutrients.js
+++ src/components/Nutrients.js
@@ -1,18 +1,17 @@
 import React from 'react'
 
-function Nutrients({nutrients, dietLabels, healthLabels}) {
+function Nutrients({nutrients}) {
   const fat = nutrients.FAT;
   const protein = nutrients.PROCNT;
   const carbs = nutrients.CHOCDF;
+  const energy = nutrients.ENERC_KCAL;
 
   return (
-    <div>
-        <p>{dietLabels}</p>
-        <p>{healthLabels}</p>
+    <div className="nutrients">
         <li>{`${fat.label}: ${Math.floor(fat.quantity)} ${fat.unit}`}</li>
         <li>{`${protein.label}: ${Math.floor(protein.quantity)} ${protein.unit}`}</li>
         <li>{`${carbs.label}: ${Math.floor(carbs.quantity)} ${carbs.unit}`}</li>
-
+        <li>{`${energy.label}: ${Math.floor(energy.quantity)} ${energy.unit}`}</li>
     </div>
   )
 }
