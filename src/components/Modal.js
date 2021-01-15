df6dccde7dc8b9113e71efb7fa31313dff3b87cb
kimcasamina <kim.csmn@gmail.com>
2021-01-15 23:52:04 +0800

    added ingredients component to modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,17 +5,9 @@ function Modal({showModal, setShowModal, ingredients, label}) {
 
   return (
     <>
-    <Ingredients ingredients={ingredients} label={label}/>
     {showModal ? (
     <div className="modalWrapper">
-      <ul className="modal">
-          <h3>{label} Recipe</h3>
-          {ingredients.map(i => {
-            return(
-              <li>{i.text}</li>
-            )
-          })}
-      </ul>
+      <Ingredients ingredients={ingredients} label={label}/>
       <button onClick={() => {setShowModal(!showModal)}}>Close</button>
     </div>
     ) : null}
